ed25b987cbfb03288e07c105778906d4b598d984
Nikolay Hizanishvili <nikolay.hizanishvili@gmail.com>
2022-12-08 23:52:17 +0300

    Add getters for Config

--- service/src/config.rs
+++ service/src/config.rs
@@ -4,30 +4,87 @@ use anyhow::Result;
 use serde_derive::Deserialize;
 
 #[derive(Deserialize)]
-pub struct Redis {
-    pub host: String,
-    pub channel: String,
-}
-
-#[derive(Deserialize)]
-pub struct Database {
-    pub host: String,
-    pub port: u64,
-    pub user: String,
-    pub pass: String,
-}
-
-#[derive(Deserialize)]
-pub struct Server {
-    pub host: String,
-    pub port: u64,
-}
-
-#[derive(Deserialize)]
 pub struct Config {
     redis: Redis,
     database: Database,
     server: Server,
+}
+
+impl Config {
+    pub fn redis(&self) -> &Redis {
+        &self.redis
+    }
+
+    pub fn database(&self) -> &Database {
+        &self.database
+    }
+
+    pub fn server(&self) -> &Server {
+        &self.server
+    }
+}
+
+#[derive(Deserialize)]
+pub struct Redis {
+    host: String,
+    channel: String,
+}
+
+impl Redis {
+    pub fn channel(&self) -> &str {
+        self.channel.as_ref()
+    }
+
+    pub fn host(&self) -> &str {
+        self.host.as_ref()
+    }
+}
+
+#[derive(Deserialize)]
+pub struct Database {
+    host: String,
+    port: u64,
+    user: String,
+    pass: String,
+    db: String,
+}
+
+impl Database {
+    pub fn host(&self) -> &str {
+        self.host.as_ref()
+    }
+
+    pub fn port(&self) -> u64 {
+        self.port
+    }
+
+    pub fn user(&self) -> &str {
+        self.user.as_ref()
+    }
+
+    pub fn pass(&self) -> &str {
+        self.pass.as_ref()
+    }
+
+    pub fn db(&self) -> &str {
+        self.db.as_ref()
+    }
+}
+
+#[derive(Deserialize)]
+pub struct Server {
+    host: String,
+    port: u64,
+}
+
+impl Server {
+    pub fn host(&self) -> &str {
+        self.host.as_ref()
+    }
+
+    pub fn port(&self) -> u64 {
+        self.port
+    }
 }
 
 impl FromStr for Config {
@@ -36,19 +93,5 @@ impl FromStr for Config {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let decoded: Config = toml::from_str(s)?;
         Ok(decoded)
-    }
-}
-
-impl Config {
-    pub fn redis(&self) -> &Redis {
-        &self.redis
-    }
-
-    pub fn db(&self) -> &Database {
-        &self.database
-    }
-
-    pub fn server(&self) -> &Server {
-        &self.server
     }
 }
